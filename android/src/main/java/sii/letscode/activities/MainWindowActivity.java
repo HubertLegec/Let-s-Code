e39ee2323ec63f99cdab4832f497e70cca77054f
HubertLegec <hubert.legec@gmail.com>
2015-11-21 00:37:54 +0100

    main window

--- android/src/main/java/sii/letscode/activities/MainWindowActivity.java
+++ android/src/main/java/sii/letscode/activities/MainWindowActivity.java
@@ -2,16 +2,28 @@ package sii.letscode.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.example.myapp.R;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by Hubert on 20.11.2015.
  */
 public class MainWindowActivity extends Activity {
+    ImageButton findButton;
+    ImageButton addButton;
+    ImageButton profileButton;
+    ImageButton settingsButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_activity);
+        setContentView(R.layout.main_window_find);
+        findButton = (ImageButton) findViewById(R.id.findButton);
+        addButton = (ImageButton) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.main_window_find);
+            }
+        });
 
     }
 }
