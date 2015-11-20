0a41e8ca4af7b199ab79943d6c88c8324a255012
woprzech <wojciech_przechodzen@wp.pl>
2015-11-20 23:21:37 +0100

    poprawa

--- server/src/main/java/book/app/server/app/controller/UserController.java
+++ server/src/main/java/book/app/server/app/controller/UserController.java
@@ -6,16 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import book.app.server.app.dto.NewUserDTO;
-import book.app.server.app.dto.UserDTO;
 import book.app.server.app.service.UserService;
 
 @Controller
