cc555b923c611d97b3d318a9d7ca0cc06088c760
HubertLegec <hubert.legec@gmail.com>
2015-11-20 22:16:41 +0100

    maven 2

---
+++ android/src/main/java/sii/letscode/activities/MainWindowActivity.java
@@ -0,0 +1,17 @@
+package sii.letscode.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.myapp.R;
+
+/**
+ * Created by Hubert on 20.11.2015.
+ */
+public class MainWindowActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+
+    }
+}
